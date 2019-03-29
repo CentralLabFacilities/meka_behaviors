6c128f857413fe8df1254324dd11c229b1905c48
meka <meka@meka-marbel.local>
2019-03-29 14:59:25 +0100

    added GazeAt state

---
+++ meka_flexbe_states/src/meka_flexbe_states/GazeAt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from flexbe_core import EventState, Logger
+from flexbe_core.proxy import ProxyPublisher
+from gaze_relay.msg import GazeRelayTarget
+
+import time
+
+
+class GazeAtTarget(EventState):
+
+    ''' Calls jacobian-control node for adaption. '''
+
+    def __init__(self, target='face', wait=None):
+
+        super(GazeAtTarget, self).__init__(outcomes = ['done', 'target_not_found'])
+
+        self._pub = ProxyPublisher('/gaze_relay/target', GazeRelayTarget)
+        self._wait = wait
+        self._target = target
+
+        self.target_map = {
+            'face':         GazeRelayTarget.FACE,
+            'neutral':      GazeRelayTarget.NEUTRAL,
+            'right_hand':   GazeRelayTarget.RIGHT_HAND,
+            'left_hand':    GazeRelayTarget.LEFT_HAND,
+            'torso':        GazeRelayTarget.TORSO,
+        }
+
+    def execute(self, d):
+
+        if self._target not in self.target_map.keys():
+            Logger.logerr('Target ' + str(self._target) + ' not found')
+            return 'target_not_found'
+
+        tar = GazeRelayTarget()
+        tar.person_id = 0
+        tar.gaze_target = self.target_map[self._target]
+
+        if self._wait is not None:
+            time.sleep(self._wait)
+
+        return 'done'
